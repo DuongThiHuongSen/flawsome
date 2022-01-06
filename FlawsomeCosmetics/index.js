6b479b8e8457762fae8255fc250a27dc2fa35e72
sen <huongsen19122001@gmail.com>
2022-01-07 00:08:39 +0700

    update

---
+++ FlawsomeCosmetics/index.js
@@ -0,0 +1,15 @@
+// fetch('./header/header.html').then((response) => response.json()).then((data) => console.log(data, 1))
+
+// fetch("./header/header.html")
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+
+$.ajax({
+    url: "./header/header.html",
+    data: {
+        zipcode: 97201
+    },
+    success: function (result) {
+        $("#header").html(result);
+    }
+});
